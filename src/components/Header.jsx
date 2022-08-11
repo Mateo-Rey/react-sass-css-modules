1c78245cd7043841b03befff900e27411fa3039a
Mateo-Rey <jrey3darts@gmail.com>
2022-08-11 16:53:17 -0400

    scss styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+export const Header = () => {
+  return (
+    <>
+      <ul className="navigation">
+        <li>
+          <a href="http://">Home</a>
+        </li>
+        <li>
+          <a href="http://">About</a>
+        </li>
+        <li>
+          <a href="http://">Contact</a>
+        </li>
+        <li>
+          <a href="http://">Links</a>
+        </li>
+      </ul>
+
+      <div className="menu">
+        <a href="" className="menu__link">Link 1</a>
+        <a href="" className="menu__link">Link 2</a>
+        <a href="" className="menu__link">Link 3</a>
+      </div>
+    </>
+  );
+};
