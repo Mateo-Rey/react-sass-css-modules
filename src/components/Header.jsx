84056bb61c7e850c6e0993266b03519ff640dd23
Mateo-Rey <jrey3darts@gmail.com>
2022-08-11 17:03:49 -0400

    USED MODULES

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,32 @@
+import styles from './Header.module.scss'
+
 export const Header = () => {
   return (
     <>
       <ul className="navigation">
         <li>
-          <a href="http://">Home</a>
+          <a>Home</a>
         </li>
         <li>
-          <a href="http://">About</a>
+          <a>About</a>
         </li>
         <li>
-          <a href="http://">Contact</a>
+          <a>Contact</a>
         </li>
         <li>
-          <a href="http://">Links</a>
+          <a>Links</a>
         </li>
       </ul>
 
       <div className="menu">
-        <a href="" className="menu__link">Link 1</a>
-        <a href="" className="menu__link">Link 2</a>
-        <a href="" className="menu__link">Link 3</a>
+        <a className="menu__link">Link 1</a>
+        <a className="menu__link">Link 2</a>
+        <a className="menu__link">Link 3</a>
+      </div>
+
+      <div className={styles['mod']}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad sed tempore quis corrupti illum aliquam ex doloribus nam eos unde hic ducimus temporibus consequatur veritatis atque, voluptatibus voluptates voluptatum earum?
+        Rem odio assumenda nobis nostrum quaerat, quis reiciendis, nemo ad quae dicta dolore harum eos quam quidem voluptate commodi repellendus delectus dolores vitae tempora praesentium ab? Illum, id? Modi, tempora.'</p>
       </div>
     </>
   );
